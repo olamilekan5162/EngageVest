3159e59b5101055f04d5fff66b838324b9156b51
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-06 09:52:44 +0100

    Add the first hero section of homepage

--- engageVest-frontend/src/routes/HomePage/HomePage.jsx
+++ engageVest-frontend/src/routes/HomePage/HomePage.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from './HomePage.module.css'
+import Button  from '../../components/Button/Button';
 
 const HomePage = () => {
+  useEffect (() => {
+    document.body.classList.add('homePageBg');
+
+    const gridOverlay = document.createElement('div');
+    gridOverlay.classList.add('grid-overlay');
+    document.body.appendChild(gridOverlay)
+
+     return () => {
+       document.body.classList.remove('homePageBg');
+       document.body.removeChild(gridOverlay)
+     };
+  }, []);
+
+ 
   return (
-    <div>HomePage</div>
-  )
+    <section className={styles.homePage_container}>
+      <div className={styles.wrapper}>
+        <h1>Powering Web3 Growth with Performance-Based Rewards</h1>
+        <p>
+          Say goodbye to wasted marketing budgets! EngageVest ensures
+          influencers and users earn based on real engagement, not fake metrics.
+          Powered by Solana smart contracts for full transparency.
+        </p>
+
+        <Button text="Connect Wallet" />
+      </div>
+    </section>
+  );
 }
 
 export default HomePage
